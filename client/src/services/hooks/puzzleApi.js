e1e6c12179b56c8e5447b9e4a53e158028e626e4
unknown <sadface_@abv.bg>
2025-03-28 23:13:53 +0200

    Add custom hooks for puzzle CRUD operations

---
+++ client/src/services/hooks/puzzleApi.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import * as requester from "../apiService";
+
+const endpoints = {
+    getAll: 'puzzles',
+    getOne: (puzzleId) => `puzzles/${puzzleId}`,
+    editPuzzle: (puzzleId) => `puzzles/${puzzleId}`
+};
+
+export const usePuzzles = () => {
+    const [puzzles, setPuzzles] = useState([]);
+
+    useEffect(() => {
+        requester.get(endpoints.getAll).
+            then(puzzles => {
+                setPuzzles(puzzles);
+            });
+    }, []);
+
+    return {
+        puzzles
+    };
+};
+
+export const usePuzzle = (puzzleId) => {
+    const [puzzle, setPuzzle] = useState({});
+
+    useEffect(() => {
+        requester.get(endpoints.getOne(puzzleId))
+            .then(puzzle => {
+                setPuzzle(puzzle);
+            });
+    }, [puzzleId]);
+
+    return {
+        puzzle
+    };
+};
+
+export const useSharePuzzle = () => {
+    const share = (puzzleData) => {
+        return requester.post(endpoints.getAll, puzzleData);
+    };
+
+    return {
+        share,
+    };
+}
+
+export const useEditPuzzle = () => {
+    const edit = (puzzleId, puzzleData) => {
+        return requester.put(endpoints.editPuzzle(puzzleId), { ...puzzleData, _id: puzzleId, });
+    };
+
+    return {
+        edit,
+    };
+};
+
+export const useDeletePuzzle = () => {
+    const remove = (puzzleId) => {
+        return requester.remove(endpoints.getOne(puzzleId), { puzzleId });
+    };
+
+    return {
+        remove,
+    };
+};
