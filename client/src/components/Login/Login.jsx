f6e040c562d81b56ed0e492cd3fd1acaa5398d6d
unknown <sadface_@abv.bg>
2025-03-28 23:18:32 +0200

    Implement Login logic for user login and context update

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,8 +1,32 @@
+import { useActionState, useContext } from 'react';
+import { useLogin } from '../../services/hooks/userApi';
 import AuthForm from '../AuthForm/AuthForm';
+import { UserContext } from '../../contexts/userContext';
+import { useNavigate } from 'react-router';
 
 export const Login = () => {
+    const navigate = useNavigate();
+    const { login } = useLogin();
+    const { authHandler } = useContext(UserContext);
+
+    const loginHandler = async (prevState, formData) => {
+        const userData = Object.fromEntries(formData);
+
+        try {
+            const loggedInUser = await login(userData);
+
+            authHandler(loggedInUser);
+
+            navigate(-1);
+        } catch (error) {
+            alert(error);
+        };
+    };
+
+    const [, loginAction, isPending] = useActionState(loginHandler);
+
     return (
-        <AuthForm isLogin={true} />
+        <AuthForm isLogin={true} formAction={loginAction} isPending={isPending} />
     );
 };
 
