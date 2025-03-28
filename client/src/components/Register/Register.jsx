f6faacdaff4b2bbfad8758d0ea0b462eb3ebc61a
unknown <sadface_@abv.bg>
2025-03-28 23:17:52 +0200

    Implement Register logic for user registration and context update

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,8 +1,33 @@
+import { useActionState, useContext } from 'react';
+import { useRegister } from '../../services/hooks/userApi';
+import { UserContext } from '../../contexts/userContext';
+
 import AuthForm from '../AuthForm/AuthForm';
 
 export const Register = () => {
+    const { register } = useRegister();
+    const { authHandler } = useContext(UserContext);
+
+    const registerHandler = async (prevState, formData) => {
+        try {
+            const userData = Object.fromEntries(formData);
+
+            const registeredUser = await register(userData);
+
+            if (!registeredUser) {
+                return;
+            }
+
+            authHandler(registeredUser);
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    const [, loginAction, isPending] = useActionState(registerHandler)
+
     return (
-        <AuthForm isLogin={false} />
+        <AuthForm isLogin={false} formAction={loginAction} isPending={isPending} />
     );
 };
 
