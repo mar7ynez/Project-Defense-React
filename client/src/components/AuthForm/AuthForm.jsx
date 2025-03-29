f25f3446f75fdbc8aa8b8aebec47d1f68a2f9284
unknown <sadface_@abv.bg>
2025-03-29 23:47:34 +0200

    Add name attribute to the input elements

--- client/src/components/AuthForm/AuthForm.jsx
+++ client/src/components/AuthForm/AuthForm.jsx
@@ -1,36 +1,37 @@
 import { Link } from 'react-router';
+
 import './authForm.css';
 
-export const AuthForm = ({ isLogin }) => {
+export const AuthForm = ({ isLogin, formAction, isPending }) => {
     return (
         <div className="form-wrapper">
-            <form className='form' action="">
+            <form className='form' action={formAction}>
                 <h2>{isLogin ? 'Sign In' : 'Sign Up'}</h2>
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="text" placeholder='Email' id='email' />
+                    <input type="text" name='email' placeholder='Email' id='email' />
                 </div>
                 {!isLogin &&
                     <div>
                         <label htmlFor="username">Username</label>
-                        <input type="text" placeholder='Username' id='username' />
+                        <input type="text" name='username' placeholder='Username' id='username' />
                     </div>
                 }
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder='Password' id='password' />
+                    <input type="password" name='password' placeholder='Password' id='password' />
                 </div>
                 {!isLogin &&
                     <div>
                         <label htmlFor="rePassword">Confirm Password</label>
-                        <input type="password" placeholder='Confirm Password' id='rePassword' />
+                        <input type="password" name='rePassword' placeholder='Confirm Password' id='rePassword' />
                     </div>
                 }
                 <div className="btn-wrapper">
-                    <button>{isLogin ? 'Sign In' : 'Sign Up'}</button>
+                    <button type='submit' disabled={isPending}>{isLogin ? 'Sign In' : 'Sign Up'}</button>
                 </div>
                 <div className='form-switch'>
-                    <p>{isLogin ? 'Dont have an account?' : 'Already have an account?'}</p>
+                    <p>{isLogin ? "Don't have an account?" : "Already have an account?"}</p>
                     <Link to={isLogin ? '/register' : '/login'}>{isLogin ? 'Sign Up' : 'Sign In'}</Link>
                 </div>
             </form >
