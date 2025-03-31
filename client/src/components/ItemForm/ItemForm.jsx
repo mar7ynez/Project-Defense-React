9dc0340f4cf30dd3c3bf094834eaf049f192e6a5
unknown <sadface_@abv.bg>
2025-03-31 23:39:20 +0300

    Integrate custom useForm hook to handle form validation

--- client/src/components/ItemForm/ItemForm.jsx
+++ client/src/components/ItemForm/ItemForm.jsx
@@ -1,3 +1,9 @@
+import { useForm } from "../../hooks/useForm";
+import { useFormValidation } from "../../hooks/useFormValidation";
+import * as itemFormConsts from '../../constants/itemFormConsts';
+
+import FormError from "../FormError/FormError";
+
 import "./itemForm.css";
 
 const ItemForm = ({ isShare,
@@ -12,39 +18,53 @@ const ItemForm = ({ isShare,
     description,
     formAction,
     isPending }) => {
+    const { initialFormData, initialFormErrors, customErrors } = itemFormConsts;
+
+    const { validate } = useFormValidation(customErrors);
+    const { touched, formErrors, changeHandler, blurHandler, isFormValid } = useForm(initialFormData, initialFormErrors, validate);
+
     return (
         <div className="wrapper">
             <div className="item-form-wrapper">
                 <h1>{isShare ? 'Share puzzle' : 'Edit Details'}</h1>
                 <form className='item-form' action={formAction}>
                     <label htmlFor="name">Puzzle Name</label>
-                    <input type="text" name="puzzleName" id="name" defaultValue={puzzleName} />
+                    <FormError touched={touched} formErrors={formErrors} name='puzzleName' />
+                    <input type="text" name="puzzleName" id="name" defaultValue={puzzleName} onChange={changeHandler} onBlur={blurHandler} />
 
                     <label htmlFor="brand">Brand</label>
-                    <input type="text" name="brand" id="brand" defaultValue={brand} />
+                    <FormError touched={touched} formErrors={formErrors} name='brand' />
+                    <input type="text" name="brand" id="brand" defaultValue={brand} onChange={changeHandler} onBlur={blurHandler} />
 
                     <label htmlFor="pieces">Pieces</label>
-                    <input type="text" name="numOfPieces" id="pieces" defaultValue={numOfPieces} />
+                    <FormError touched={touched} formErrors={formErrors} name='numOfPieces' />
+                    <input type="text" name="numOfPieces" id="pieces" defaultValue={numOfPieces} onChange={changeHandler} onBlur={blurHandler} />
 
                     <label htmlFor="thickness">Thickness</label>
-                    <input type="text" name="puzzleThickness" id="thickness" defaultValue={puzzleThickness} />
+                    <FormError touched={touched} formErrors={formErrors} name='puzzleThickness' />
+                    <input type="text" name="puzzleThickness" id="thickness" defaultValue={puzzleThickness} onChange={changeHandler} onBlur={blurHandler} />
 
                     <label htmlFor="size">Size</label>
-                    <input type="text" name="puzzleSize" id="size" defaultValue={puzzleSize} />
+                    <FormError touched={touched} formErrors={formErrors} name='puzzleSize' />
+                    <input type="text" name="puzzleSize" id="size" defaultValue={puzzleSize} onChange={changeHandler} onBlur={blurHandler} />
 
                     <label htmlFor="material">Material</label>
-                    <input type="text" name="material" id="material" defaultValue={material} />
+                    <FormError touched={touched} formErrors={formErrors} name='material' />
+                    <input type="text" name="material" id="material" defaultValue={material} onChange={changeHandler} onBlur={blurHandler} />
 
                     <label htmlFor="age">Age</label>
-                    <input type="text" name="age" id="age" defaultValue={age} />
+                    <FormError touched={touched} formErrors={formErrors} name='age' />
+                    <input type="text" name="age" id="age" defaultValue={age} onChange={changeHandler} onBlur={blurHandler} />
 
                     <label htmlFor="imageUrl">Image URL</label>
-                    <input type="text" name="imageUrl" id="imageUrl" defaultValue={imageUrl} />
+                    <FormError touched={touched} formErrors={formErrors} name='imageUrl' />
+                    <input type="text" name="imageUrl" id="imageUrl" defaultValue={imageUrl} onChange={changeHandler} onBlur={blurHandler} />
 
                     <label htmlFor="description">Description</label>
-                    <textarea name="description" id="description" defaultValue={description} />
+                    <FormError touched={touched} formErrors={formErrors} name='description' />
+                    <textarea name="description" id="description" defaultValue={description} onChange={changeHandler} onBlur={blurHandler} />
 
-                    <button type="submit" disabled={isPending}>{isShare ? 'Share' : 'Edit'}</button>
+                    <button className={isShare && !isFormValid(false) || isPending ? 'disabled' : ''} type="submit">{isShare ? 'Share' : 'Edit'}</button>
                 </form>
             </div>
         </div>
