257172211ce1d49a45612b32a23f990454415d0f
unknown <sadface_@abv.bg>
2025-03-31 23:48:15 +0300

    Update Navigation component to conditionally display auth links

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,25 +1,44 @@
 import { Link } from 'react-router';
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/userContext';
+
+import Profile from '../Profile/Profile';
+
 import './Navigation.css';
 
 const Navigation = () => {
+    const { accessToken } = useContext(UserContext);
+    const isAuthenticated = accessToken;
+
     return (
-        <>
-            <nav className="nav">
-                <div className="logo">
-                    <Link to={'/'}>uniqueone</Link>
+        <nav className="nav">
+            <div className="logo">
+                <Link to={'/'}>PuzzleHub</Link>
+            </div>
+            <ul>
+                <li className="nav-link"><Link to={'/'}>Home</Link></li>
+                <li className="nav-link"><Link to={'/explore'}>Explore</Link></li>
+
+                {isAuthenticated &&
+                    <li className="nav-link"><Link to={'/share'}>Share</Link></li>
+                }
+
+                <li className="nav-link"><Link to={'/about'}>About</Link></li>
+            </ul>
+
+            {!isAuthenticated &&
+                <div className="auth-links-wrapper">
+                    <div className='auth-link'>
+                        <Link to={'/login'}>Sign In</Link>
+                    </div>
+                    <div className='auth-link'>
+                        <Link to={'/register'}>Sign Up</Link>
+                    </div>
                 </div>
-                <ul>
-                    <li className="nav-link"><Link to={'/'}>Home</Link></li>
-                    <li className="nav-link"><Link to={'/explore'}>Explore</Link></li>
-                    <li className="nav-link"><Link to={'/login'}>Login</Link></li>
-                    <li className="nav-link"><Link to={'/register'}>Register</Link></li>
-                    <li className="nav-link"><Link to={'/about'}>About</Link></li>
-                </ul>
-                <div className="side-attr">
-                    <img className="profile" src="https://cdn-icons-png.flaticon.com/512/3237/3237472.png" alt="" />
-                </div>
-            </nav>
-        </>
+            }
+
+            {isAuthenticated && <Profile />}
+        </nav>
     );
 };
 
