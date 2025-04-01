10ef597867766afd7249da0634d6f15ae95d92fa
unknown <sadface_@abv.bg>
2025-04-02 00:01:29 +0300

    Set up routing and authentication guards for app navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,21 +9,41 @@ import Explore from "./components/Explore/Explore";
 import About from "./components/About/About";
 import Register from "./components/Register/Register";
 import Details from "./components/Details/Details";
+import Share from "./components/Share/Share";
+import Edit from "./components/Edit/Edit";
+import Logout from "./components/Logout/Logout";
+import MyPuzzles from "./components/MyPuzzles/MyPuzzles";
+import EditProfile from "./components/EditProfile/EditProfile";
+
+import UserProvider from "./providers/UserProvider";
+import AuthGuard from "./guards/AuthGuard";
+import GuestGuard from "./guards/GuestGuard";
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <Navigation />
       <Particle />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/about" element={<About />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/details" element={<Details />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/explore/:puzzleId/details" element={<Details />} />
+
+        <Route element={<GuestGuard />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        <Route element={<AuthGuard />}>
+          <Route path="/share" element={<Share />} />
+          <Route path="/profile" element={<EditProfile />} />
+          <Route path="/my-puzzles" element={<MyPuzzles />} />
+          <Route path="/explore/:puzzleId/edit" element={<Edit />} />
+          <Route path="/logout" element={<Logout />} />
+        </Route>
       </Routes>
-    </>
+    </UserProvider>
   );
 };
 
