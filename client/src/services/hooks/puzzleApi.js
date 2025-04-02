d0893bb43cc199acb485924afc61211f8127a2cc
unknown <sadface_@abv.bg>
2025-04-02 23:53:28 +0300

    Add custom hooks for like and top puzzles

--- client/src/services/hooks/puzzleApi.js
+++ client/src/services/hooks/puzzleApi.js
@@ -5,7 +5,10 @@ const endpoints = {
     getAll: 'puzzles',
     getMyPuzzles: 'puzzles/my-puzzles',
     getOne: (puzzleId) => `puzzles/${puzzleId}`,
-    editPuzzle: (puzzleId) => `puzzles/${puzzleId}`
+    editPuzzle: (puzzleId) => `puzzles/${puzzleId}`,
+    likePuzzle: (puzzleId) => `puzzles/${puzzleId}/like`,
+    getLikes: (puzzleId) => `puzzles/${puzzleId}/likes`,
+    getTopPuzzles: 'puzzles/top',
 };
 
 export const usePuzzles = () => {
@@ -39,17 +42,25 @@ export const useMyPuzzles = () => {
 };
 
 export const usePuzzle = (puzzleId) => {
-    const [puzzle, setPuzzle] = useState({});
+    const [puzzle, setPuzzle] = useState({});  // Start with null, not an empty object
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        setIsLoading(true);
         requester.get(endpoints.getOne(puzzleId))
-            .then(puzzle => {
+            .then((puzzle) => {
                 setPuzzle(puzzle);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                alert(error.message);
             });
     }, [puzzleId]);
 
     return {
-        puzzle
+        puzzle,
+        isLoading,
     };
 };
 
@@ -70,6 +81,63 @@ export const useEditPuzzle = () => {
 
     return {
         edit,
+    };
+};
+
+export const useLike = (puzzleId) => {
+    const [likes, setLikes] = useState([]);
+    const [isLiking, setIsLiking] = useState(false);
+
+    useEffect(() => {
+        requester.get(endpoints.getLikes(puzzleId))
+            .then(allLikes => {
+                const { likes } = allLikes;
+                setLikes(likes);
+            });
+    }, [puzzleId]);
+
+    const like = (userId, puzzleId) => {
+        setIsLiking(true);
+        return requester.post(endpoints.likePuzzle(puzzleId), { userId })
+            .then(() => {
+                requester.get(endpoints.getLikes(puzzleId))
+                    .then(allLikes => {
+                        const { likes } = allLikes;
+
+                        setLikes(likes);
+                        setIsLiking(false);
+                    })
+                    .catch(error => {
+                        console.log(error);
+
+                        setLikes((prevState) => prevState.filter(id => id !== userId));
+                        setIsLiking(false);
+                    });
+            });
+    };
+
+    return {
+        like,
+        likes,
+        isLiking
+    };
+};
+
+export const useTopPuzzles = () => {
+    const [topPuzzles, setTopPuzzles] = useState([]);
+
+    useEffect(() => {
+        requester.get(endpoints.getTopPuzzles)
+            .then(topPuzzles => {
+                setTopPuzzles(topPuzzles)
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    return {
+        topPuzzles,
     };
 };
 
