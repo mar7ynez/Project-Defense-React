f34a68b650d30e37c1707de3a41936b3401322b5
unknown <sadface_@abv.bg>
2025-04-03 00:02:02 +0300

    Rename _id prop to puzzleId for clarity

--- client/src/components/ExploreItem/ExploreItem.jsx
+++ client/src/components/ExploreItem/ExploreItem.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router";
 
 import './exploreItem.css';
 
-const ExploreItem = ({ _id, puzzleName, imageUrl }) => {
+const ExploreItem = ({ _id: puzzleId, puzzleName, imageUrl }) => {
     return (
         <div className="item">
-            <Link to={`/explore/${_id}/details`}>
+            <Link to={`/explore/${puzzleId}/details`}>
                 <div className="details-wrapper">
                     <img src={imageUrl} alt="Puzzle Name" />
                     <p>{puzzleName}</p>
