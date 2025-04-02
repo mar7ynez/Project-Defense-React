eaa5bce642e3334753d0450dcafb018f8cdb61bd
unknown <sadface_@abv.bg>
2025-04-03 00:03:51 +0300

    Enhance puzzle details page: Add like functionality, handle errors, and toast notifications

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,55 +1,70 @@
 import { Link, useNavigate, useParams } from 'react-router';
-import { useDeletePuzzle, usePuzzle } from '../../services/hooks/puzzleApi';
-
-import DetailsList from './DetailsList/DetailsList';
-
-import './details.css';
+import { useDeletePuzzle, useLike, usePuzzle } from '../../services/hooks/puzzleApi';
 import { useContext } from 'react';
 import { UserContext } from '../../contexts/userContext';
+
+import DetailsList from './DetailsList/DetailsList';
+import { toast } from 'react-toastify';
+
+import './details.css';
 
 const Details = () => {
     const navigate = useNavigate();
     const { puzzleId } = useParams();
     const { puzzle } = usePuzzle(puzzleId);
     const { remove } = useDeletePuzzle();
-    const { _id, accessToken } = useContext(UserContext);
+    const { _id: userId, accessToken } = useContext(UserContext);
+    const { like, likes, isLiking } = useLike(puzzleId);
 
-    const isOwner = puzzle._ownerId === _id;
-
-    const {
-        puzzleName,
-        brand,
-        numOfPieces,
-        puzzleThickness,
-        puzzleSize,
-        material,
-        age,
-        description,
-        imageUrl } = puzzle;
+    const isOwner = puzzle._ownerId === userId;
+    const hasLiked = likes.includes(userId);
 
     const detailsContent = {
-        Brand: brand,
-        Pieces: numOfPieces,
-        Thickness: puzzleThickness,
-        Size: puzzleSize,
-        Material: material,
-        Age: age
-    }
+        Brand: puzzle.brand,
+        Pieces: puzzle.numOfPieces,
+        Thickness: puzzle.puzzleThickness,
+        Size: puzzle.puzzleSize,
+        Material: puzzle.material,
+        Age: puzzle.age
+    };
 
     const deleteHandler = async () => {
-        await remove(puzzleId);
+        try {
+            await remove(puzzleId);
 
-        navigate('/explore');
-    }
+            navigate('/explore');
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
+
+    const likeHandler = async () => {
+        try {
+            if (!userId) {
+                return;
+            }
+
+            await like(userId, puzzleId);
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
 
     return (
         <>
             <section className="details">
                 <div className="image">
-                    <img src={imageUrl} alt={puzzleName} />
+                    <img src={puzzle.imageUrl} alt={puzzle.puzzleName} />
+                    <div className='like-content'>
+                        {!hasLiked && !isOwner && accessToken &&
+                            <button className={isLiking ? 'disabled' : ''} onClick={likeHandler}><i className="fa-solid fa-thumbs-up"></i></button>
+                        }
+                        <i className="fa-solid fa-heart">{likes.length}</i>
+                    </div>
                 </div>
                 <div className="details-content">
-                    <h1>{puzzleName}</h1>
+                    <h1>{puzzle.puzzleName}</h1>
 
                     <DetailsList detailsContent={detailsContent} />
 
@@ -63,7 +78,7 @@ const Details = () => {
             </section>
             <section className='description'>
                 <h1>Description</h1>
-                <p>{description}</p>
+                <p>{puzzle.description}</p>
             </section>
         </>
     );
